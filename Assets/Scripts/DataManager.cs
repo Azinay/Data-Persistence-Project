2e5db1b761c4ff0fcf021f5e1c5ab780f278b22b
Azinay <vojtihivskijowork@gmail.com>
2022-08-25 16:35:51 +0300

    Data Storage
    Best score is saved between sessions

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
@@ -15,9 +16,36 @@ public class DataManager : MonoBehaviour
     DontDestroyOnLoad(gameObject);
   }
 
+  [System.Serializable]
+  class SaveData
+  {
+    public int BestScore;
+  }
+
+  void Start()
+  {
+    string path = $"{Application.persistentDataPath}/savefile.json";
+
+    if(File.Exists(path))
+    {
+      string json = File.ReadAllText(path);
+      SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+      BestScore = data.BestScore;
+    }
+  }
+
   public void BestScoreRecorder(int m_Points)
   {
     if(m_Points > BestScore)
+    {
       BestScore = m_Points;
+
+      SaveData data = new SaveData();
+      data.BestScore = BestScore;
+      string json = JsonUtility.ToJson(data);
+
+      File.WriteAllText($"{Application.persistentDataPath}/savefile.json", json);
+    }
   }
 }
